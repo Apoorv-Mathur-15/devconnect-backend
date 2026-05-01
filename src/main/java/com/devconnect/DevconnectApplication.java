cae77afc5a217d7299ae114b947511c249c5f253
Apoorv-Mathur-15 <mathur.apoorv367@gmail.com>
2026-05-01 11:54:07 +0530

    Force Hibernate to create tables and ensure entity scanning is correctly configured

--- src/main/java/com/devconnect/DevconnectApplication.java
+++ src/main/java/com/devconnect/DevconnectApplication.java
@@ -2,8 +2,10 @@ package com.devconnect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.devconnect.entity")
 public class DevconnectApplication {
 
 	public static void main(String[] args) {
