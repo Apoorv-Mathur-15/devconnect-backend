78cb0d1e44df5f5e4e6410e02232048731067385
Apoorv-Mathur-15 <mathur.apoorv367@gmail.com>
2026-05-02 11:29:18 +0530

    Add PostController with endpoints for creating and fetching posts

---
+++ src/main/java/com/devconnect/controller/PostController.java
@@ -0,0 +1,33 @@
+package com.devconnect.controller;
+
+import com.devconnect.entity.Post;
+import com.devconnect.service.PostService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping
+    public Post createPost(@RequestBody String content) {
+        return postService.createPost(content);
+    }
+
+    @GetMapping
+    public List<Post> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+    @GetMapping("/me")
+    public Post getMyPosts() {
+        return postService.getMyPosts();
+    }
+}
