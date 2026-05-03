586c86edd69422311de415583dd61cffb4a26262
Apoorv-Mathur-15 <mathur.apoorv367@gmail.com>
2026-05-03 21:10:14 +0530

    Upgrade AsyncService to support parallel processing using CompletableFuture

--- src/main/java/com/devconnect/service/AsyncService.java
+++ src/main/java/com/devconnect/service/AsyncService.java
@@ -3,21 +3,53 @@ package com.devconnect.service;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.CompletableFuture;
+
 @Service
 public class AsyncService {
 
-    @Async
-    public void processPostAsync(String content, String email) {
+    @Async("taskExecutor")
+    public CompletableFuture<String> processContent(String content) {
+
+        System.out.println("Thread: " +  Thread.currentThread().getName());
 
         try {
-            System.out.println("Async task started for email: " + email);
-
-            Thread.sleep(3000);
-
-            System.out.println("Processed post: " + content);
+            Thread.sleep(2000);
         }
         catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        System.out.println("Processed content");
+
+        return CompletableFuture.completedFuture("Content Done");
+    }
+
+    @Async("taskExecutor")
+    public CompletableFuture<String> sendNotification(String email) {
+        System.out.println("Thread: " +  Thread.currentThread().getName());
+        try {
+            Thread.sleep(1500);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Notification sent to: " + email);
+        return CompletableFuture.completedFuture("Notification Done");
+    }
+
+    @Async("taskExecutor")
+    public CompletableFuture<String> saveAnalytics(String email) {
+        System.out.println("Thread: " +  Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(1000);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Analytics saved for " + email);
+
+        return CompletableFuture.completedFuture("Analytics Done");
     }
 }
